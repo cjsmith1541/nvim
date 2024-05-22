c29e73feab8d1b970d7a7ec0e936b1a93e8875e0
Charlie Smith <cjsmith12345@hotmail.com>
2024-05-23 00:04:12 +0100

    new

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,96 +1,94 @@
-require("obsidian").setup({
-	opts = {
-		workspaces = {
-			{
-				name = "Notes",
-				path = "/home/cjsmith/Obsidian/Main",
-			},
-		},
-		completion = {
-			nvim_cmp = true,
-			min_chars = 2,
-		},
-		new_notes_location = "notes_   s",
-		wiki_link_func = function(opts)
-			if opts.id == nil then
-				return string.format("[[%s]]", opts.label)
-			elseif opts.label ~= opts.id then
-				return string.format("[[%s|%s]]", opts.id, opts.label)
-			else
-				return string.format("[[%s]]", opts.id)
-			end
-		end,
-
-		daily_notes = {
-			-- Optional, if you keep daily notes in a separate directory.
-			folder = "dailies",
-			-- Optional, if you want to change the date format for the ID of daily notes.
-			date_format = "%Y-%m-%d",
-			-- Optional, if you want to change the date format of the default alias of daily notes.
-			alias_format = "%B %-d, %Y",
-			-- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-			template = nil,
-		},
-
-		mappings = {
-			-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-			["gf"] = {
-				action = function()
-					return require("obsidian").util.gf_passthrough()
-				end,
-				opts = { noremap = false, expr = true, buffer = true },
-			},
-			-- Toggle check-boxes.
-			["<leader>ch"] = {
-				action = function()
-					return require("obsidian").util.toggle_checkbox()
-				end,
-				opts = { buffer = true },
-			},
-			-- Smart action depending on context, either follow link or toggle checkbox.
-			["<cr>"] = {
-				action = function()
-					return require("obsidian").util.smart_action()
-				end,
-				opts = { buffer = true, expr = true },
-			},
-		},
-		note_frontmatter_func = function(note)
-			-- This is equivalent to the default frontmatter function.
-			local out = { id = note.id, aliases = note.aliases, tags = note.tags, area = "", project = "" }
-
-			-- `note.metadata` contains any manually added fields in the frontmatter.
-			-- So here we just make sure those fields are kept in the frontmatter.
-			if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-				for k, v in pairs(note.metadata) do
-					out[k] = v
-				end
-			end
-			return out
-		end,
-
-		note_id_func = function(title)
-			-- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-			-- In this case a note with the title 'My new note' will be given an ID that looks
-			-- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
-			local suffix = ""
-			if title ~= nil then
-				-- If title is given, transform it into valid file name.
-				suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-			else
-				-- If title is nil, just add 4 random uppercase letters to the suffix.
-				for _ = 1, 4 do
-					suffix = suffix .. string.char(math.random(65, 90))
-				end
-			end
-			return tostring(os.time()) .. "-" .. suffix
-		end,
-
-		templates = {
-			subdir = "Templates",
-			date_format = "%Y-%m-%d-%a",
-			time_format = "%H:%M",
-			tags = "",
+opts = {
+	workspaces = {
+		{
+			name = "Notes",
+			path = "/home/cjsmith/Obsidian/Main",
 		},
 	},
-})
+	completion = {
+		nvim_cmp = true,
+		min_chars = 2,
+	},
+	new_notes_location = "notes_   s",
+	wiki_link_func = function(opts)
+		if opts.id == nil then
+			return string.format("[[%s]]", opts.label)
+		elseif opts.label ~= opts.id then
+			return string.format("[[%s|%s]]", opts.id, opts.label)
+		else
+			return string.format("[[%s]]", opts.id)
+		end
+	end,
+
+	daily_notes = {
+		-- Optional, if you keep daily notes in a separate directory.
+		folder = "dailies",
+		-- Optional, if you want to change the date format for the ID of daily notes.
+		date_format = "%Y-%m-%d",
+		-- Optional, if you want to change the date format of the default alias of daily notes.
+		alias_format = "%B %-d, %Y",
+		-- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+		template = nil,
+	},
+
+	mappings = {
+		-- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
+		["gf"] = {
+			action = function()
+				return require("obsidian").util.gf_passthrough()
+			end,
+			opts = { noremap = false, expr = true, buffer = true },
+		},
+		-- Toggle check-boxes.
+		["<leader>ch"] = {
+			action = function()
+				return require("obsidian").util.toggle_checkbox()
+			end,
+			opts = { buffer = true },
+		},
+		-- Smart action depending on context, either follow link or toggle checkbox.
+		["<cr>"] = {
+			action = function()
+				return require("obsidian").util.smart_action()
+			end,
+			opts = { buffer = true, expr = true },
+		},
+	},
+	note_frontmatter_func = function(note)
+		-- This is equivalent to the default frontmatter function.
+		local out = { id = note.id, aliases = note.aliases, tags = note.tags, area = "", project = "" }
+
+		-- `note.metadata` contains any manually added fields in the frontmatter.
+		-- So here we just make sure those fields are kept in the frontmatter.
+		if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+			for k, v in pairs(note.metadata) do
+				out[k] = v
+			end
+		end
+		return out
+	end,
+
+	note_id_func = function(title)
+		-- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+		-- In this case a note with the title 'My new note' will be given an ID that looks
+		-- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+		local suffix = ""
+		if title ~= nil then
+			-- If title is given, transform it into valid file name.
+			suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+		else
+			-- If title is nil, just add 4 random uppercase letters to the suffix.
+			for _ = 1, 4 do
+				suffix = suffix .. string.char(math.random(65, 90))
+			end
+		end
+		return tostring(os.time()) .. "-" .. suffix
+	end,
+
+	templates = {
+		subdir = "Templates",
+		date_format = "%Y-%m-%d-%a",
+		time_format = "%H:%M",
+		tags = "",
+	},
+}
